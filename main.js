e53168762183b1ce5e99f432fbd5a8755a15347a
DONELL ELLIS <DONELL.ELLIS@GMAIL.COM>
2018-10-23 15:51:29 -0400

    level 1 sequence working. state before adding setTimer

--- main.js
+++ main.js
@@ -28,47 +28,62 @@ playButton.addEventListener('click', function () {
 })
 
 
+
 // click color buttons
-buttons[0].addEventListener('click', function () {
-    console.log("red button selected")
-})
+// buttons[0].addEventListener('click', function () {
+//     console.log("red button selected")
+// })
+buttons[0].addEventListener('click', changeButtonBackgroundRed)
 
-buttons[1].addEventListener('click', function () {
-    console.log("blue button selected")
-})
+buttons[1].addEventListener('click', changeButtonBackgroundBlue)
 
-buttons[2].addEventListener('click', function () {
-    console.log("yellow button selected")
-})
+buttons[2].addEventListener('click', changeButtonBackgroundYellow)
 
-buttons[3].addEventListener('click', function () {
-    console.log("green button selected")
-})
+buttons[3].addEventListener('click', changeButtonBackgroundGreen)
+
+
+
+
 
 // change color button colors
-// function changeButtonBackgroundRed () {
-//     selectRed.style.backgroundColor = 'magenta';
-// }
+function changeButtonBackgroundRed () {
+    buttons[0].style.opacity = .5;
+    buttons[0].style.opacity = "";
+}
 
+function changeButtonBackgroundBlue () {
+    buttons[1].style.opacity = .5;
+}
 
+function changeButtonBackgroundYellow () {
+    buttons[2].style.opacity = .5;
+}
 
+function changeButtonBackgroundGreen () {
+    buttons[3].style.opacity = .5;
+}
 
+// display sequence Level 1
+function displaySequenceLevel1 () {
+    for (let i = 0; i < sequenceLevel1.length; i++) {
+        checkValues(sequenceLevel1[i]) 
+    }
+}
 
+// checkes values in the sequence array
+function checkValues (button) {
+    // console.log(button)
+    if (button.className === 'button red' ) {
+        return changeButtonBackgroundRed ()
+    }
+    else if (button.className === 'button blue') {
+        return changeButtonBackgroundBlue ()
+    }
+    else if (button.className === 'button yellow') {
+        return changeButtonBackgroundYellow ()
+    }
+    else if (button.className === 'button green') {
+        return changeButtonBackgroundGreen ()
+    }
+}
 
-
-
-// function changeBackgroundColorToBlue () {
-//     // No need to change the content of this function. Don't worry if this code is new to you.
-//     document.body.style.backgroundColor = 'blue';
-//   }
-  
-//   function changeBackgroundColorToRed () {
-//     // No need to change the content of this function. Don't worry if this code is new to you.
-//     document.body.style.backgroundColor = 'red';
-//   }
-  
-//   const buttonRed = document.querySelector('.red')
-//   buttonRed.addEventListener('click', changeBackgroundColorToRed)
-  
-//   const buttonBlue = document.querySelector('.blue')
-//   buttonBlue.addEventListener('click', changeBackgroundColorToBlue)
