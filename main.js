23ab75b38e5facf8e703838cdfac43a0e6b6d677
DONELL ELLIS <DONELL.ELLIS@GMAIL.COM>
2018-10-23 11:36:33 -0400

    added color and play buttons

--- main.js
+++ main.js
@@ -3,9 +3,6 @@
 // When the user clicks play the computer:
 // -starts at level 1
 // -selects three random buttons from red, blue, green, and yellow
-// 	-are the color buttons in an array? or are they selected by Querry Selector All?
-// 	-if it were not random, how would I get the computer to select the buttons in sequence?
-// 		-cycle through the buttons which are in an array
 // 		-for each button, illuminate it (CSS feature—opacity?)
 // -gets the buttons to light-up on the console
 // -on delay
@@ -18,4 +15,34 @@ let sequenceLevel1 = [buttons[0], buttons[2], buttons[3], buttons[2]]
 
 console.log(sequenceLevel1)
 
-document.querySelector(.)
+let playButton = document.querySelector(".buttonPlay")
+
+console.log(playButton)
+
+// click play
+playButton.addEventListener('click', function () {
+    console.log(sequenceLevel1)
+})
+
+// color buttons
+let selectRed = document.querySelector(".red")
+let selectBlue = document.querySelector(".blue")
+let selectYellow = document.querySelector(".yellow")
+let selectGreen = document.querySelector(".green")
+
+// click color buttons
+selectRed.addEventListener('click', function () {
+    console.log("red button selected")
+})
+
+selectBlue.addEventListener('click', function () {
+    console.log("blue button selected")
+})
+
+selectYellow.addEventListener('click', function () {
+    console.log("yellow button selected")
+})
+
+selectGreen.addEventListener('click', function () {
+    console.log("green button selected")
+})
