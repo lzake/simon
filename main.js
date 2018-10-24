d80847b1d5a07113a3e9e64dfc38c5d9aa22a390
DONELL ELLIS <DONELL.ELLIS@GMAIL.COM>
2018-10-23 22:06:52 -0400

    made color indicator functions more dry and completed timers

--- main.js
+++ main.js
@@ -1,89 +1,102 @@
-// function gameInit
-
-// When the user clicks play the computer:
-// -starts at level 1
-// -selects three random buttons from red, blue, green, and yellow
-// 		-for each button, illuminate it (CSS feature—opacity?)
-// -gets the buttons to light-up on the console
-// -on delay
-
-
 
 // defines color buttons in array
 let buttons = document.querySelectorAll(".button")
 
-
 // defines level 1 sequence
 let sequenceLevel1 = [buttons[0], buttons[2], buttons[3], buttons[2]]
 
+// defines level 1 user response
+let userResponseLevel1 = []
 
 // defines play button
 let playButton = document.querySelector(".buttonPlay")
 
+// click handler for play button
+playButton.addEventListener('click', function(){showIndicatorsInSequence(sequenceLevel1)})
 
 
-// click play
-playButton.addEventListener('click', function () {
-    console.log(sequenceLevel1)
-})
+// click handlers for color buttons
+buttons[0].addEventListener('click', function() { handleColorButtonPress(buttons[0]) })
+
+buttons[1].addEventListener('click', function() { handleColorButtonPress(buttons[1]) })
+
+buttons[2].addEventListener('click', function() { handleColorButtonPress(buttons[2]) })
+
+buttons[3].addEventListener('click', function() { handleColorButtonPress(buttons[3]) })
 
 
-
-// click color buttons
-// buttons[0].addEventListener('click', function () {
-//     console.log("red button selected")
-// })
-buttons[0].addEventListener('click', changeButtonBackgroundRed)
-
-buttons[1].addEventListener('click', changeButtonBackgroundBlue)
-
-buttons[2].addEventListener('click', changeButtonBackgroundYellow)
-
-buttons[3].addEventListener('click', changeButtonBackgroundGreen)
-
-
-
-
-
-// change color button colors
-function changeButtonBackgroundRed () {
-    buttons[0].style.opacity = .5;
-    buttons[0].style.opacity = "";
+function setButtonOpacityFull(colorButton) {
+    colorButton.style.opacity = 1;
 }
 
-function changeButtonBackgroundBlue () {
-    buttons[1].style.opacity = .5;
+function setButtonOpacityHalf(colorButton) {
+    colorButton.style.opacity = 0.5;
 }
 
-function changeButtonBackgroundYellow () {
-    buttons[2].style.opacity = .5;
+function handleColorButtonPress(colorButton) {
+    setButtonOpacityHalf(colorButton);
+    setTimeout(
+        function() {
+            setButtonOpacityFull(colorButton)
+        }, 200
+    )
+
+    // store click order by pushing colorButton to array
+    userResponseLevel1.push(colorButton);
+    checkForMatch()
 }
 
-function changeButtonBackgroundGreen () {
-    buttons[3].style.opacity = .5;
-}
 
-// display sequence Level 1
-function displaySequenceLevel1 () {
-    for (let i = 0; i < sequenceLevel1.length; i++) {
-        checkValues(sequenceLevel1[i]) 
+function checkForMatch() {
+    // todo checkForMatch not working find a way to evaluate a match
+    if (sequenceLevel1 === userResponseLevel1) {
+        alert('You win!')
+    } else {
+        console.log(sequenceLevel1, userResponseLevel1)
     }
 }
 
-// checkes values in the sequence array
-function checkValues (button) {
-    // console.log(button)
-    if (button.className === 'button red' ) {
-        return changeButtonBackgroundRed ()
-    }
-    else if (button.className === 'button blue') {
-        return changeButtonBackgroundBlue ()
-    }
-    else if (button.className === 'button yellow') {
-        return changeButtonBackgroundYellow ()
-    }
-    else if (button.className === 'button green') {
-        return changeButtonBackgroundGreen ()
+
+
+function showColorButtonIndicator(colorButton) {
+    setButtonOpacityHalf(colorButton);
+    setTimeout(
+        function() {
+            setButtonOpacityFull(colorButton)
+        }, 500
+    )
+}
+
+
+
+function showIndicatorsInSequence(levelArray) {
+    for (let i = 0; i < levelArray.length; i++) {
+        let delay = (500 * i) + 500;
+        setTimeout( function() {
+            showColorButtonIndicator(levelArray[i])
+        }, delay)
     }
 }
 
+
+
+// checks values in the sequence array
+// function showSequence(button) {
+//     console.log(button)
+//     if (button.className === 'button red' ) {
+//         console.log('red')
+//         changeButtonBackgroundRed()
+//     }
+//     else if (button.className === 'button blue') {
+//         console.log('blue')
+//         changeButtonBackgroundBlue()
+//     }
+//     else if (button.className === 'button yellow') {
+//         console.log('yellow')
+//         changeButtonBackgroundYellow()
+//     }
+//     else if (button.className === 'button green') {
+//         console.log('green')
+//         changeButtonBackgroundGreen()
+//     }
+// }
