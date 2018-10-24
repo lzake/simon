2fd84cc6d376c40ab481488cbb55e6d1108b16b6
DONELL ELLIS <DONELL.ELLIS@GMAIL.COM>
2018-10-24 16:47:36 -0400

    game functioning at mvp level

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 let buttons = document.querySelectorAll(".button")
 
 // defines level 1 sequence
-let sequenceLevel1 = [buttons[0], buttons[2], buttons[3], buttons[2]]
+const sequenceLevel1 = [buttons[0], buttons[2], buttons[3], buttons[2]]
 
 // defines level 1 user response
 let userResponseLevel1 = []
@@ -11,18 +11,18 @@ let userResponseLevel1 = []
 // defines play button
 let playButton = document.querySelector(".buttonPlay")
 
-// click handler for play button
+// event listener and handler for play button
 playButton.addEventListener('click', function(){showIndicatorsInSequence(sequenceLevel1)})
 
 
-// click handlers for color buttons
-buttons[0].addEventListener('click', function() { handleColorButtonPress(buttons[0]) })
+// event listeners and handlers for color buttons
+buttons[0].addEventListener('click', function() { handleUserColorButtonPress(buttons[0]) })
 
-buttons[1].addEventListener('click', function() { handleColorButtonPress(buttons[1]) })
+buttons[1].addEventListener('click', function() { handleUserColorButtonPress(buttons[1]) })
 
-buttons[2].addEventListener('click', function() { handleColorButtonPress(buttons[2]) })
+buttons[2].addEventListener('click', function() { handleUserColorButtonPress(buttons[2]) })
 
-buttons[3].addEventListener('click', function() { handleColorButtonPress(buttons[3]) })
+buttons[3].addEventListener('click', function() { handleUserColorButtonPress(buttons[3]) })
 
 
 function setButtonOpacityFull(colorButton) {
@@ -33,7 +33,7 @@ function setButtonOpacityHalf(colorButton) {
     colorButton.style.opacity = 0.5;
 }
 
-function handleColorButtonPress(colorButton) {
+function handleUserColorButtonPress(colorButton) {
     setButtonOpacityHalf(colorButton);
     setTimeout(
         function() {
@@ -43,22 +43,38 @@ function handleColorButtonPress(colorButton) {
 
     // store click order by pushing colorButton to array
     userResponseLevel1.push(colorButton);
-    checkForMatch()
+    if (userResponseLevel1.length === sequenceLevel1.length) {
+        checkForMatch()
+    }
+    
+    // to do resolve error. cannot compare arrays. check for match should happen after the user input equals sequenceLevel1.length
 }
 
 
 function checkForMatch() {
-    // todo checkForMatch not working find a way to evaluate a match
-    if (sequenceLevel1 === userResponseLevel1) {
+    // computer defined
+   for (let i = 0; i < sequenceLevel1.length; i++) {
+       let stringSeqLevel1 = []
+       stringSeqLevel1.push(sequenceLevel1[i].classList[1])
+       joinedSeqLevel1 = stringSeqLevel1.join()
+   }
+//    user input
+   for (let i = 0; i < userResponseLevel1.length; i++) {
+       let stringUserRespLevel1 = []
+       stringUserRespLevel1.push(userResponseLevel1[i].classList[1])
+       joinedUserRespLevel1 = stringUserRespLevel1.join()
+   }
+
+    console.log('test')
+    if (joinedSeqLevel1 === joinedUserRespLevel1) {
         alert('You win!')
     } else {
-        console.log(sequenceLevel1, userResponseLevel1)
+        alert('Try again!')
     }
 }
 
 
-
-function showColorButtonIndicator(colorButton) {
+function flashColorButtonIndicator(colorButton) {
     setButtonOpacityHalf(colorButton);
     setTimeout(
         function() {
@@ -68,35 +84,11 @@ function showColorButtonIndicator(colorButton) {
 }
 
 
-
 function showIndicatorsInSequence(levelArray) {
     for (let i = 0; i < levelArray.length; i++) {
         let delay = (500 * i) + 500;
         setTimeout( function() {
-            showColorButtonIndicator(levelArray[i])
+            flashColorButtonIndicator(levelArray[i])
         }, delay)
     }
 }
-
-
-
-// checks values in the sequence array
-// function showSequence(button) {
-//     console.log(button)
-//     if (button.className === 'button red' ) {
-//         console.log('red')
-//         changeButtonBackgroundRed()
-//     }
-//     else if (button.className === 'button blue') {
-//         console.log('blue')
-//         changeButtonBackgroundBlue()
-//     }
-//     else if (button.className === 'button yellow') {
-//         console.log('yellow')
-//         changeButtonBackgroundYellow()
-//     }
-//     else if (button.className === 'button green') {
-//         console.log('green')
-//         changeButtonBackgroundGreen()
-//     }
-// }
