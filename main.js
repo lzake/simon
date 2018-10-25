40ce58febba8e4379133b569af9e0fd95ce1fc19
DONELL ELLIS <DONELL.ELLIS@GMAIL.COM>
2018-10-25 17:14:20 -0400

    added comments back to main.js

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
+
+
+// defines color buttons in array
 let buttons = document.querySelectorAll(".button")
 
+
+// computer generated level 1 sequence
 let sequenceLevel1 = []
 for (let i = 0; i <= 3; i++) {
     sequenceLevel1.push(buttons[Math.floor(Math.random() * buttons.length)])
 }   
 
+
+// defines level 1 user response
 let userResponseLevel1 = []
 
+
+// defines play button
 let playButton = document.querySelector(".buttonPlay")
 
-playButton.addEventListener('click', function(){showIndicatorsInSequence(sequenceLevel1) })
 
+// event listener and handler for play button
+playButton.addEventListener('click', function(){showIndicatorsInSequence(sequenceLevel1)})
+
+
+// event listeners and handlers for color buttons
 buttons[0].addEventListener('click', function() { receiveUserColorButtonPress(buttons[0]) })
 
 buttons[1].addEventListener('click', function() { receiveUserColorButtonPress(buttons[1]) })
@@ -19,13 +32,16 @@ buttons[2].addEventListener('click', function() { receiveUserColorButtonPress(bu
 
 buttons[3].addEventListener('click', function() { receiveUserColorButtonPress(buttons[3]) })
 
+
 function setButtonOpacityFull(colorButton) {
     colorButton.style.opacity = 1;
 }
 
+
 function setButtonOpacityHalf(colorButton) {
     colorButton.style.opacity = 0.5;
 }
+
 
 function receiveUserColorButtonPress(colorButton) {
     setButtonOpacityHalf(colorButton);
@@ -35,11 +51,14 @@ function receiveUserColorButtonPress(colorButton) {
         }, 200
     )
 
+    // store click order by pushing colorButton to array
     userResponseLevel1.push(colorButton);
     if (userResponseLevel1.length === sequenceLevel1.length) {
         checkForMatch()
     }
+    
 }
+
 
 function checkForMatch() {
    for (let i = 0; i < sequenceLevel1.length; i++) {
@@ -61,6 +80,7 @@ function checkForMatch() {
     }
 }
 
+
 function flashColorButtonIndicator(colorButton) {
     setButtonOpacityHalf(colorButton);
     setTimeout(
@@ -69,6 +89,7 @@ function flashColorButtonIndicator(colorButton) {
         }, 500
     )
 }
+
 
 function showIndicatorsInSequence(levelArray) {
     for (let i = 0; i < levelArray.length; i++) {
